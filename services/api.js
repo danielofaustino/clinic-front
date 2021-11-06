6fb018bfddc9d2f07a9af1600fa214acc3c5fc67
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-11-06 19:13:33 -0300

    url deploy

--- services/api.js
+++ services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const API_URL = 'https://clinic-front-seven.vercel.app';
 
 const api = axios.create({
   baseURL: API_URL,
